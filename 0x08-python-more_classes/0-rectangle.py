c3e00bfc06cc4fa90bddcda996eebb78669d3a15
Kibugi1 <njugunabrian843@gmail.com>
2023-07-03 04:35:18 -0700

    Task 0: Write an empty class Rectangle that defines a rectangle:

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a class called Rectanglr."""
+
+
+class Rectangle:
+"""Represents a Rectangle."""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+This directory contains code files for more python classes and object Tasks in ALX
